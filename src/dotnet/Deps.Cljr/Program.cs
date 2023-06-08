88c677e8ecdb082770838695a92b58dbdfafe862
David Miller <dmiller2718@gmail.com>
2023-06-07 21:34:07 -0400

    create test project

---
+++ src/dotnet/Deps.Cljr/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Deps.Cljr;
+
+
+
+internal class Program
+{
+    static int Main(string[] args)
+    {
+        foreach (string arg in args)
+        {
+            Console.WriteLine(arg);
+        }
+
+        return 0;
+    }
+
+
+    enum EMode { Repl, Main, Exec, Tool }
+
+    static void ParseArgs(string[] args)
+    {
+
+    }
+
+}
